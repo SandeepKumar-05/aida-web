554391129c66bccf154e7e8b86bcba239ef146b2
TejasRanjith <tejasranjith035611@gmail.com>
2025-01-16 00:29:18 +0530

    add Header and Intro components with styling; update logo image

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,13 +1,13 @@
 import './Header.css';
-import pic from './assets/react.svg'
+import pic from './assets/logo.png';
 
 
 function Header() {
     
     return (
             <header>
-                <img src={pic} alt="Docplanner Group" className='image' />
                 <nav>
+                    <img src={pic} alt="Docplanner Group" className='image' />
                     <ul className='navlist'>
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Career</a></li>
