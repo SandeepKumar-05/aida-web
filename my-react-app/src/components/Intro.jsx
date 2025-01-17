097c5d81766550b189b976b08191d9bffde9e24b
TejasRanjith <tejasranjith035611@gmail.com>
2025-01-17 23:14:03 +0530

    add react-type-animation dependency; update Para component for dynamic content display

--- my-react-app/src/components/Intro.jsx
+++ my-react-app/src/components/Intro.jsx
@@ -10,6 +10,7 @@ function Intro() {
                 <img src={introImage} alt="AIDA Logo" className='introImage' />
             </div>
             <Para content="Welcome to the Artificial Intelligence and Data Science Association!We are thrilled to welcome you to our community of innovators, thinkers, and problem-solvers. AIDA is dedicated to creating a vibrant platform where AI and data science professionals, enthusiasts, and students can connect, collaborate, and grow together. Whether you're looking to expand your knowledge, advance your career, or contribute to the evolution of these transform."></Para>
+            <Para content="Let's have a look on what's happening on AIDA" indexImg={2}></Para>
         </>
     );
 }
--- my-react-app/src/components/Para.jsx
+++ my-react-app/src/components/Para.jsx
@@ -1,15 +1,50 @@
 import './Para.css';
 import ChatBot from './assets/chatbot.png';
+import {TypeAnimation} from 'react-type-animation';
 function Para(props) {
-
+    if (props.indexImg === 1) {
+        return (
+            <div className='tester1'>
+                <p>{props.content}</p>
+                <img src={ChatBot} alt="BOT" className='ChatBot' />
+            </div>
+        );
+    }
+    else if (props.indexImg === 2) {
+        return (
+            <div className='tester2'>
+                <img src={ChatBot} alt="BOT" className='ChatBot2' />
+                <p>{props.content}</p>
+            </div>
+        );
+    }
+    else{
     return(
         <>
-            <div className='IntroText'>
+            <div className='tester0'>
                 <img src={ChatBot} alt="BOT" className='ChatBot' />
-                <p>{props.content}</p>
+                <TypeAnimation
+                className='TypeAnimation' 
+                splitter={
+                    (str) => str.split(/(?= )/)
+                }
+                sequence={
+                    [
+                        props.content,
+                    ]
+                }
+                speed={{ type: 'keyStrokeDelayInMs',value: 30 }}
+                style={{fontSize: '1.5em', color: 'white'}}
+                deletionSpeed={0}
+                repeat={true}
+                wrapper='span'
+                />
+
+                {/* <p>{props.content}</p> */}
             </div>
         </>
     );
+    }
 }
 
 
