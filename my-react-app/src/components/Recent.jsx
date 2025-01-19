6d260f6255fd8122cb1b7bb5f43904480c823655
TejasRanjith <tejasranjith035611@gmail.com>
2025-01-19 23:32:40 +0530

    add Tailwind CSS files and .npmignore entries; configure module exports

--- my-react-app/src/components/Recent.jsx
+++ my-react-app/src/components/Recent.jsx
@@ -1,34 +1,61 @@
 import "./Recent.css";
-import Slider from 'react-slick';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Para from './Para';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-const datas=[
-    {
-        title:"Tejas",
-        age:25,
-    },
-    {
-        title:"Tejabhai",
-        age:35,
-    },
-    {
-        title:"TRuTH",
-        age:45,
-    }
-]
+import pic from './assets/event.png';
+import { TypeAnimation } from "react-type-animation";
 
 function Recent() {
     return (
-        // <div>
-        //     <div>
-        //         <Slider>
-
-        //         </Slider>
-        //     </div>
-        // </div>
-        <h1 className="text-9xl">
-      Hello world!
-    </h1>
+      <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slide">
+          <img className="eventImg" src={pic} alt="pic"></img>
+          <Para  indexImg={1} fontsize="22px" content="Al Amthan R. M. (S6 AD), qualified from the South-West Zone Inter-Uni-  versity Karate Competition to represent APJAKTU in the All India  Inter-University Karate Competition 2024-25 at Maharshi Dayanand Uni-  versity, Rohtak.Know more!"/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+          <img className="eventImg" src={pic} alt="pic"></img>
+          <Para  indexImg={1} fontsize="22px" content="Al Amthan R. M. (S6 AD), qualified from the South-West Zone Inter-Uni-  versity Karate Competition to represent APJAKTU in the All India  Inter-University Karate Competition 2024-25 at Maharshi Dayanand Uni-  versity, Rohtak.Know more!"/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+          <img className="eventImg" src={pic} alt="pic"></img>
+          <Para  indexImg={1} fontsize="22px" content="Al Amthan R. M. (S6 AD), qualified from the South-West Zone Inter-Uni-  versity Karate Competition to represent APJAKTU in the All India  Inter-University Karate Competition 2024-25 at Maharshi Dayanand Uni-  versity, Rohtak.Know more!"/>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide">
+          <img className="eventImg" src={pic} alt="pic"></img>
+          <Para  indexImg={1} fontsize="22px" content="Al Amthan R. M. (S6 AD), qualified from the South-West Zone Inter-Uni-  versity Karate Competition to represent APJAKTU in the All India  Inter-University Karate Competition 2024-25 at Maharshi Dayanand Uni-  versity, Rohtak.Know more!"/>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      </>
     );
 }
 
